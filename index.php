014669c24aa97b7a18a91148a07db70090298d97
Mohamed Imad <email2emad@gmail.com>
2020-09-02 14:11:47 +0500

    created an index file

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css">
+    
+    <title>Snippets</title>
+</head>
+<body>
+    <h4>Code Snippets</h4>
+    Heres a thing I came up with. Lot's of reusable code snippets from different languages. Visitors may contribute by adjusting my snippets or even submitting your snippets to be published here. Creative ideas are always welcome.
+
+
+    <ul>
+        <li><a href="time_ago.php">Time ago function</a></li>
+        <li>More coming soon...</li>
+    </ul>
+</body>
+</html>
