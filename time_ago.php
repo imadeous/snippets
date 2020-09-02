6f389706d5dfe23e9e950d0540dc018bfb18248a
Mohamed Imad <email2emad@gmail.com>
2020-09-02 13:38:57 +0500

    added time ago function

---
+++ time_ago.php
@@ -0,0 +1,32 @@
+<?php
+function time_ago($time) {
+	$now = time();					//current time (change timezone accordingly)
+	$period = $now - $time;			//get the time difference
+	$refs = [
+		'year' 		=> '31553280',	// (365+365+365+365+365)/5 * 24 * 60 * 60
+		'month' 	=> '2629440',	// ((365+365+365+365+365)/5/12) * 24 * 60 * 60
+		'week' 		=> '604800',	// 7 * 24 * 60 * 60
+		'day' 		=> '86400',		// 24 * 60 * 60
+		'hour' 		=> '3600',		// 60 * 60
+		'minute' 	=> '60',		// 1 * 60
+		'second' 	=> '1'			// 1
+	];
+
+	foreach ($refs as $ref => $value) {
+		$ago = $period / $value;	//convert each reference to seconds
+			if($ago >= 1) { 		//all positive numbers
+				$ago = floor($ago); //round down the value
+				$str =  $ago . " " . $ref . ($ago > 1 ? 's' : '') . " ago";
+				$data[] = $str;
+			} else if ($ago == 0) { //exception for 0 which is unlikely
+				$data[] = "Just now";
+			} else if ($ago < 0){ 				//negative time is only possible with time travel
+				$data[] = "Time traveller detected";
+			}
+	}
+	echo $data[0];
+}
+
+$time = strtotime("2020-02-17 17:44:00");
+time_ago($time);
+?>
