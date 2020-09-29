161f738a34139f71104a9483bd129aa84fc7bc5b
Mohamed Imad <email2emad@gmail.com>
2020-09-30 01:55:01 +0500

    final final

--- Cart_v0.2.php
+++ Cart_v0.2.php
@@ -1,5 +1,4 @@
 <?php 
-error_reporting(0);
 session_start();
 class Cart 
 {
@@ -17,7 +16,7 @@ class Cart
 					'name'	   => $name,
 					'quantity' => $_SESSION['cart'][$id]['quantity'] + $quantity,
 					'price'    => $price,
-					'subtotal' => ($_SESSION['cart'][$id]['quantity'] + $quantity) * $price,
+					'subtotal' => round(($_SESSION['cart'][$id]['quantity'] + $quantity) * $price, 2),
 					'tax'	   => $tax
 				];
 		} else {
@@ -27,7 +26,7 @@ class Cart
 				'name'	   => $name,
 				'quantity' => $quantity, 
 				'price'    => $price,
-				'subtotal' => $quantity * $price,
+				'subtotal' => round($quantity * $price, 2),
 				'tax'	   => $tax
 			];
 		}
@@ -67,27 +66,35 @@ class Cart
 	public function subtotal()
 	{
 		$subtotal = 0;
-		foreach ($_SESSION['cart'] as $item) {
-			$subtotal += round($item['subtotal'], 2);
+		if(isset($_SESSION['cart'])) {
+			foreach ($_SESSION['cart'] as $item => $value) {
+				if(array_key_exists($item, $_SESSION['cart']) && $item != 'grand_total' && $item != 'tax_total'&& $item != 'subtotal') {
+					$subtotal += $_SESSION['cart'][$item]['subtotal'];
+				}
+			}
+			return round($subtotal, 2);
 		}
-		return $subtotal;
 	}
 
 	//retrun the sum of all item taxes
 	public function tax_total()
 	{
 		$tax_total = 0;
-		foreach ($_SESSION['cart'] as $item) {
-			$tax_total += round($item['subtotal'] * $item['tax'], 2);
+		if(isset($_SESSION['cart'])) {
+			foreach ($_SESSION['cart'] as $item => $value) {
+				if(array_key_exists($item, $_SESSION['cart']) && $item != 'grand_total' && $item != 'tax_total'&& $item != 'subtotal') {
+					$tax_total += $_SESSION['cart'][$item]['subtotal'] * $_SESSION['cart'][$item]['tax'];
+				}
+			}
+			return round($tax_total, 2);
 		}
-		return $tax_total;
 	}
 
 	//retrun the grand total
 	public function grand_total()
 	{
-		$grand_total = round($this->subtotal() + $this->tax_total(), 2);
-		return $grand_total;
+		$grand_total = $this->subtotal() + $this->tax_total();
+		return round($grand_total, 2);
 	}
 
 
@@ -102,7 +109,12 @@ class Cart
 
 	public function load()
 	{
-		return json_encode(array_reverse($_SESSION['cart']));
+		if(isset($_SESSION['cart'])) {
+			return json_encode(array_reverse($_SESSION['cart']));
+		} else {
+			return null;
+		}
+		
 	}
 
 }
@@ -112,10 +124,11 @@ class Cart
 $Cart = new Cart();
 
 //add function call(taxed)
-$Cart->add("LOREM", "Tuna", 1, 100, 0.06);
+$Cart->add("LOREM", "Tuna", 2, 100, 0.06);
 
 //add function call(non-taxed)
-$Cart->add("IPSUM", "Skipjack", 1, 100);
+$Cart->add("IPSUM", "Skipjack", 1, 100.54);
+$Cart->add("Dolor", "Mahi Mahi", 2, 28.39, 0.06);
 
 //remove item function call
 // $Cart->remove("LOREM");
